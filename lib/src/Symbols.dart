f54c70a3cdfc0bce9cb83afbd1fe216b63a8680e
Viktor Dakalov <atlantidu@gmail.com>
2015-01-18 13:38:08 +0600

    Implements table

--- lib/src/Symbols.dart
+++ lib/src/Symbols.dart
@@ -11,11 +11,42 @@ class Symbols {
   static const String doubleLineAngleTopRight = "\u2557";
   static const String doubleLineAngleBottomLeft = "\u255A";
   static const String doubleLineAngleBottomRight = "\u255D";
-  static const String doubleLineFromTop = "\u2566";
-  static const String doubleLineFromLeft = "\u2560";
-  static const String doubleLineFromRight = "\u2563";
-  static const String doubleLineFromBottom = "\u2569";
+  static const String doubleLineFromTop = "\u2569";
+  static const String doubleLineFromLeft = "\u2563";
+  static const String doubleLineFromRight = "\u2560";
+  static const String doubleLineFromBottom = "\u2566";
   static const String doubleLineCross = "\u256C";
+  static const String doubleLineSingleTop = "\u2567";
+  static const String doubleLineSingleLeft = "\u2562";
+  static const String doubleLineSingleRight = "\u255F";
+  static const String doubleLineSingleBottom = "\u2564";
+
+  static const String singleLineVertical = "\u2502";
+  static const String singleLineHorizontal = "\u2500";
+  static const String singleLineCross = "\u253C";
+  static const String singleLineAngleTopLeft = "\u250C";
+  static const String singleLineAngleTopRight = "\u2510";
+  static const String singleLineAngleBottomLeft = "\u2514";
+  static const String singleLineAngleBottomRight = "\u2518";
+  static const String singleLineFromTop = "\u2534";
+  static const String singleLineFromLeft = "\u2524";
+  static const String singleLineFromRight = "\u251C";
+  static const String singleLineFromBottom = "\u252C";
+
+  static const String singleLineHeavyHorizontal = "\u2501";
+  static const String singleLineHeavyVertical = "\u2503";
+  static const String singleLineHeavyAngleTopLeft = "\u250F";
+  static const String singleLineHeavyAngleTopRight = "\u2513";
+  static const String singleLineHeavyAngleBottomLeft = "\u2517";
+  static const String singleLineHeavyAngleBottomRight = "\u251B";
+  static const String singleLineHeavyFromTop = "\u253B";
+  static const String singleLineHeavyFromLeft = "\u252B";
+  static const String singleLineHeavyFromRight = "\u2523";
+  static const String singleLineHeavyFromBottom = "\u2533";
+  static const String singleLineHeavyFromTopLight = "\u2537";
+  static const String singleLineHeavyFromLeftLight = "\u2528";
+  static const String singleLineHeavyFromRightLight = "\u2520";
+  static const String singleLineHeavyFromBottomLight = "\u252F";
 
   static const String dashLineHorizontal = "\u2508";
   static const String dashLineVertical = "\u250A";
--- lib/src/displays/Table.dart
+++ lib/src/displays/Table.dart
@@ -4,173 +4,293 @@ const _BUFFER_OUTPUT = 0;
 const _BUFFER_COLUMNS_WIDTH = 1;
 const _BUFFER_TABLE_WIDTH = 2;
 const _BUFFER_CURRENT_ROW = 3;
-const _BUFFER_CURRENT_CELL = 4;
+const _BUFFER_CURRENT_COLUMN = 4;
+const _BUFFER_TIME_INIT = 5;
+const _BUFFER_TIME_TABLE = 6;
+const _BUFFER_TIME_ROWS = 7;
+const _BUFFER_TIME_ALL = 8;
 
-class Table<K, V> {
+class Table<C, R> {
 
-  final List<K> columns = [];
-  final List<List<V>> rows = new List<List<V>>();
+  final List<C> columns = new List<C>();
+  final List<R> data = new List<R>();
+  final Map<int, dynamic> _buffers = new Map<int, dynamic>();
 
-  final Map<int, dynamic> _buffer = {
-    _BUFFER_OUTPUT: "",
-    _BUFFER_COLUMNS_WIDTH: 0,
-    _BUFFER_TABLE_WIDTH: 0,
-    _BUFFER_CURRENT_ROW: 0,
-    _BUFFER_CURRENT_CELL: 0
-  };
+  int size;
+  bool verbose = false;
 
-  Table();
-
-  Table.fromData(List<List<V>> rows) {
-    this.rows.addAll(rows);
-  }
-
-  void _updateColumnsWidthBuffer() {
-
-    List<int> sizes = new List<int>();
-    int index = 0, totalWidth = 0;
-
-    rows.forEach((cells){
-      index = 0;
-      cells.forEach((cell){
-        if (index >= sizes.length) {
-          sizes.add(0);
-        }
-        if (cell.toString().length > sizes[index]) {
-          sizes[index] = cell.toString().length;
-          totalWidth += sizes[index];
-        }
-        index++;
-      });
-    });
-
-    _buffer[_BUFFER_COLUMNS_WIDTH] = sizes;
-    _buffer[_BUFFER_TABLE_WIDTH] = totalWidth;
-  }
-
-  void _bufferAdd(String char, {int num: 1}) {
-    for (; num > 0; num--) {
-      _buffer[_BUFFER_OUTPUT] += char;
+  Table.fromCollumns(List<C> columns_) {
+    if (columns_ is List<C>) {
+      columns.addAll(columns_);
     }
   }
 
-  void _bufferEOL() {
-    _bufferAdd(Symbols.CL);
+  Table.fromData(this.size, List<R> rows_) {
+    if (rows_ is List<R>) {
+      data.addAll(rows_);
+    }
+  }
+
+  void _initBuffers() {
+
+    List<int> sizes = new List<int>();
+    int totalWidth = 0;
+
+    for (int i = 0; i < columns.length; i++) {
+      String value = columns[i].toString();
+
+      if (i >= sizes.length) {
+        sizes.add(0);
+      }
+
+      if (value.length > sizes[i]) {
+        sizes[i] = value.length;
+      }
+    }
+
+    for (int i = 0; i < data.length; i++) {
+      int _i = i % size;
+      String value = data[i].toString();
+
+      if (_i >= sizes.length) {
+        sizes.add(0);
+      }
+
+      if (value.length > sizes[_i]) {
+        sizes[_i] = value.length;
+      }
+    }
+
+    sizes.forEach((size){ totalWidth += size; });
+
+    _buffers[_BUFFER_OUTPUT] = new List<String>();
+    _buffers[_BUFFER_COLUMNS_WIDTH] = sizes;
+    _buffers[_BUFFER_TABLE_WIDTH] = totalWidth;
+  }
+
+  void _bufferOutputAdd(String char, {int num: 1}) {
+    for (; num > 0; num--) {
+      _buffers[_BUFFER_OUTPUT].add(char);
+    }
+  }
+
+  void _bufferOutputAddLn(String char, {int num: 1}) {
+    _bufferOutputAdd(char, num: num);
+    _bufferOutputEOL();
+  }
+
+  void _bufferOutputEOL() {
+    _bufferOutputAdd(Symbols.CL);
   }
 
   void _tableCell(String cell) {
-    int width = _buffer[_BUFFER_COLUMNS_WIDTH][_buffer[_BUFFER_CURRENT_CELL]];
+    int width = _buffers[_BUFFER_COLUMNS_WIDTH][_buffers[_BUFFER_CURRENT_COLUMN]];
 
-    _bufferAdd(cell);
-    _bufferAdd(Symbols.space, num: width - cell.length);
-
+    _bufferOutputAdd(cell);
+    _bufferOutputAdd(Symbols.space, num: width - cell.length);
   }
 
   void _tableBegin() {
 
-    _updateColumnsWidthBuffer();
-    _bufferAdd(Symbols.doubleLineAngleTopLeft);
+    _bufferOutputAdd(Symbols.singleLineAngleTopLeft);
 
-    for (int i = 0; i < _buffer[_BUFFER_COLUMNS_WIDTH].length; i++) {
+    for (int i = 0; i < _buffers[_BUFFER_COLUMNS_WIDTH].length; i++) {
       if (i > 0) {
-        _bufferAdd(Symbols.doubleLineFromTop);
+        _bufferOutputAdd(Symbols.singleLineFromBottom);
       }
-      _bufferAdd(
-          Symbols.doubleLineHorizontal,
-          num: _buffer[_BUFFER_COLUMNS_WIDTH][i] + 2);
+      _bufferOutputAdd(
+          Symbols.singleLineHorizontal,
+          num: _buffers[_BUFFER_COLUMNS_WIDTH][i] + 2);
     }
-    _bufferAdd(Symbols.doubleLineAngleTopRight);
-    _bufferEOL();
+    _bufferOutputAddLn(Symbols.singleLineAngleTopRight);
   }
 
   void _tableEnd() {
 
-    _bufferAdd(Symbols.doubleLineAngleBottomLeft);
+    _bufferOutputAdd(Symbols.singleLineAngleBottomLeft);
 
-    for (int i = 0; i < _buffer[_BUFFER_COLUMNS_WIDTH].length; i++) {
+    for (int i = 0; i < _buffers[_BUFFER_COLUMNS_WIDTH].length; i++) {
       if (i > 0) {
-        _bufferAdd(Symbols.doubleLineFromBottom);
+        _bufferOutputAdd(Symbols.singleLineFromTop);
       }
-      _bufferAdd(
-          Symbols.doubleLineHorizontal,
-          num: _buffer[_BUFFER_COLUMNS_WIDTH][i] + 2);
+      _bufferOutputAdd(
+          Symbols.singleLineHorizontal,
+          num: _buffers[_BUFFER_COLUMNS_WIDTH][i] + 2);
     }
-    _bufferAdd(Symbols.doubleLineAngleBottomRight);
+    _bufferOutputAddLn(Symbols.singleLineAngleBottomRight);
+  }
+
+  void _tableHeader() {
+    _bufferOutputAdd(Symbols.singleLineVertical);
+    _bufferOutputAdd(Symbols.space);
+
+    for (int i = 0; i < columns.length; i++) {
+      _buffers[_BUFFER_CURRENT_COLUMN] = i;
+      _tableCell(columns[i].toString().toUpperCase());
+      if (columns.last != columns[i]) {
+        _tableHeaderCellSeparator();
+      }
+    }
+
+    _bufferOutputAdd(Symbols.space);
+    _bufferOutputAddLn(Symbols.singleLineVertical);
+
+    _tableHeaderSeparator();
+  }
+
+  void _tableHeaderSeparator() {
+
+    _bufferOutputAdd(Symbols.singleLineFromRight);
+
+    for (int i = 0; i < _buffers[_BUFFER_COLUMNS_WIDTH].length; i++) {
+      if (i > 0) {
+        _bufferOutputAdd(Symbols.singleLineCross);
+      }
+      _bufferOutputAdd(
+          Symbols.singleLineHorizontal,
+          num: _buffers[_BUFFER_COLUMNS_WIDTH][i] + 2);
+    }
+
+    _bufferOutputAddLn(Symbols.singleLineFromLeft);
+  }
+
+  void _tableHeaderCellSeparator() {
+    _bufferOutputAdd(Symbols.space);
+    _bufferOutputAdd(Symbols.singleLineVertical);
+    _bufferOutputAdd(Symbols.space);
   }
 
   void _tableCellSeparator() {
-    _bufferAdd(Symbols.space);
-    _bufferAdd(Symbols.doubleLineVertical);
-    _bufferAdd(Symbols.space);
+    _bufferOutputAdd(Symbols.space);
+    _bufferOutputAdd(Symbols.singleLineVertical);
+    _bufferOutputAdd(Symbols.space);
   }
 
-  void _tableRow(List<dynamic> row) {
-    _bufferAdd(Symbols.doubleLineVertical);
-    _bufferAdd(Symbols.space);
+  void _tableRow(List<R> row) {
+    _bufferOutputAdd(Symbols.singleLineVertical);
+    _bufferOutputAdd(Symbols.space);
 
     for (int i = 0; i < row.length; i++) {
-      _buffer[_BUFFER_CURRENT_CELL] = i;
+      _buffers[_BUFFER_CURRENT_COLUMN] = i;
       _tableCell(row[i].toString());
       if (row.last != row[i]) {
         _tableCellSeparator();
       }
     }
 
-    _bufferAdd(Symbols.space);
-    _bufferAdd(Symbols.doubleLineVertical);
-    _bufferEOL();
+    _bufferOutputAdd(Symbols.space);
+    _bufferOutputAddLn(Symbols.singleLineVertical);
   }
 
   void _tableRowSeparator() {
 
-    _bufferAdd(Symbols.doubleLineFromLeft);
+    _bufferOutputAdd(Symbols.singleLineFromRight);
 
-    for (int i = 0; i < _buffer[_BUFFER_COLUMNS_WIDTH].length; i++) {
+    for (int i = 0; i < _buffers[_BUFFER_COLUMNS_WIDTH].length; i++) {
       if (i > 0) {
-        _bufferAdd(Symbols.doubleLineCross);
+        _bufferOutputAdd(Symbols.singleLineCross);
       }
-      _bufferAdd(
-          Symbols.doubleLineHorizontal,
-          num: _buffer[_BUFFER_COLUMNS_WIDTH][i] + 2);
+      _bufferOutputAdd(
+          Symbols.singleLineHorizontal,
+          num: _buffers[_BUFFER_COLUMNS_WIDTH][i] + 2);
     }
 
-    _bufferAdd(Symbols.doubleLineFromRight);
-    _bufferEOL();
+    _bufferOutputAddLn(Symbols.singleLineFromLeft);
   }
 
-  void subTable(int startRow, int numRow, {int startCell, int numCell}) {
+  void _timeStart(int index) {
+    _buffers[index] = new DateTime.now();
+  }
 
-    List sublist = rows.sublist(startRow, startRow + numRow);
+  void _timeEnd(int index) {
+    _buffers[index] = new DateTime.now().difference(_buffers[index]);
+  }
 
-    if (startCell > 0 && numCell > 0) {
-      sublist = sublist.map((row){
-        return row.sublist(startCell, startCell + numCell);
-      });
-    }
+  void _additionalInfo() {
+    _bufferOutputAddLn("render time:");
+    _bufferOutputAddLn(" - all: ${_buffers[_BUFFER_TIME_ALL].inMilliseconds}ms");
+    _bufferOutputAddLn(" - init: ${_buffers[_BUFFER_TIME_INIT].inMilliseconds}ms");
+    _bufferOutputAddLn(" - table: ${(_buffers[_BUFFER_TIME_TABLE] - _buffers[_BUFFER_TIME_ROWS]).inMilliseconds}ms");
+    _bufferOutputAddLn(" - rows: ${_buffers[_BUFFER_TIME_ROWS].inMilliseconds}ms");
+    _bufferOutputAddLn(" - row: ${_buffers[_BUFFER_TIME_ROWS].inMilliseconds/(data.length/size)}ms");
+  }
 
-    rows.clear();
-    rows.addAll(sublist);
+  Table crop(int startRow, int numRow) {
+
+    startRow *= size;
+    numRow *= size;
+
+    List sublist = data.sublist(startRow, startRow + numRow);
+
+    data.clear();
+    data.addAll(sublist);
+
+    return this;
   }
 
   Table clone() {
-    return new Table.fromData(rows);
+    return new Table.fromData(size, data);
   }
 
   String toString() {
 
-    _tableBegin();
+    if (verbose) {
+      _timeStart(_BUFFER_TIME_ALL);
+      _timeStart(_BUFFER_TIME_INIT);
+    }
 
-    for (int i = 0; i < rows.length; i++) {
-      _buffer[_BUFFER_CURRENT_ROW] = i;
-      _tableRow(rows[i]);
-      if (rows.last != rows[i]) {
-        _tableRowSeparator();
+    if (size == null) {
+      if (columns.length > 0) {
+        size = columns.length;
+      } else {
+        return "Log (Table): Укажите количество колонок (свойство size)";
       }
+    }
+
+    _initBuffers();
+
+    if (verbose) {
+      _timeEnd(_BUFFER_TIME_INIT);
+      _timeStart(_BUFFER_TIME_TABLE);
+    }
+
+    _tableBegin();
+    _tableHeader();
+
+    int len = data.length;
+    data.length = data.length - (data.length % size);
+
+    if (verbose) {
+      _timeStart(_BUFFER_TIME_ROWS);
+    }
+
+    for (int i = 0; i < data.length; i += size) {
+      if (data.length >= i + size) {
+        _buffers[_BUFFER_CURRENT_ROW] = i;
+        _tableRow(data.sublist(i, i + size));
+        if (i + size < data.length) {
+          _tableRowSeparator();
+        }
+      }
+    }
+
+    if (verbose) {
+      _timeEnd(_BUFFER_TIME_ROWS);
     }
 
     _tableEnd();
 
-    return _buffer[_BUFFER_OUTPUT];
+    if (verbose) {
+      _timeEnd(_BUFFER_TIME_TABLE);
+      _timeEnd(_BUFFER_TIME_ALL);
+    }
+
+    if (verbose) {
+      _bufferOutputAddLn("redundant cells: ${len - data.length}");
+      _additionalInfo();
+    }
+
+    return _buffers[_BUFFER_OUTPUT].join();
   }
 
 }
