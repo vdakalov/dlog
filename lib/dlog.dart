a313e62db9198fc70ff20eb8b89b917ff435e96a
Viktor Dakalov <atlantidu@gmail.com>
2015-01-22 09:22:53 +0500

    Implements Time

--- lib/dlog.dart
+++ lib/dlog.dart
@@ -8,3 +8,4 @@ part "src/Display.dart";
 part "src/displays/Table.dart";
 part "src/displays/Tree.dart";
 part "src/displays/Json.dart";
+part "src/displays/Time.dart";
---
+++ lib/src/displays/Time.dart
@@ -0,0 +1,41 @@
+part of dlog;
+
+class Time extends _Display {
+
+  String description;
+
+  List<String> _actionNames = new List<String>();
+  List<DateTime> _actionStamps = new List<DateTime>();
+
+  Time([this.description = "undescribed"]);
+
+  _registerAction(String description) {
+    _actionNames.add(description);
+    _actionStamps.add(new DateTime.now());
+  }
+
+  init() {
+    _registerAction("init");
+  }
+
+  check(String description, [Function doit]) {
+    _registerAction(description);
+  }
+
+  checkFunc(String description, Function doit) {
+    doit();
+    _registerAction(description);
+  }
+
+  @override
+  _initBuffer() {
+
+    _outputBufferWriteLn(this.description);
+
+    for (int i = 1; i < _actionNames.length; i++) {
+      Duration diff = _actionStamps[i].difference(_actionStamps[i-1]);
+      _outputBufferWriteLn("$i. ${diff} - ${_actionNames[i]}");
+    }
+
+  }
+}
