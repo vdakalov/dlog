a49a45f948e5ae43f128473733b54017c33d80cf
Viktor Dakalov <atlantidu@gmail.com>
2015-01-22 09:25:11 +0500

    Fix library name for json exmaple

--- example/json.dart
+++ example/json.dart
@@ -1,7 +1,7 @@
 // Copyright (c) 2015, Viktor Dakalov. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-library dlog.example;
+library dlog.example.json;
 
 import "package:dlog/dlog.dart" as dlog;
 import "dart:convert";
